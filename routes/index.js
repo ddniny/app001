27cd03430add778af717ec1a45180abc05724dfe
JiaXun <jiaxun_jx@126.com>
2012-08-31 09:39:51 +0800

    commit application

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+    var now = new Date();
+    var jsdom = require('jsdom');
+    
+    jsdom.env(
+        'http://www.china.com.cn',
+        ['http://code.jquery.com/jquery-1.8.0.min.js'],
+        function(errors, window) {
+            var list = [];
+            window.$('a').each(function(i, e) {
+                list.push(window.$(e).attr('href'));
+            });
+            res.render('index', { 'title': 'Test', 'alist': list });
+        }
+    );
 };
